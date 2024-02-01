220dd9a5d8844f3a16ab6cddc79cacffd3053a85
Victor <maurogonzalezvictor@gmail.com>
2024-02-01 14:18:42 +0100

    Prueba responsive Index sin modulo

--- asociacionJuvenil/src/components/footer/footer.tsx
+++ asociacionJuvenil/src/components/footer/footer.tsx
@@ -36,30 +36,31 @@ function Footer() {
     return (
         <footer className='bg-grey'>
             <Container className='pt-2 pb-0'>
-
                 <Row className='m-2 mb-3 justify-content-center'>
                     <Col className='col text-center' xs='1' md={1}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width={iconSize} height={iconSize} fill="currentColor" className="bi bi-twitter-x" viewBox="0 0 16 16">
-                            <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
-                        </svg>
+                        <a href={REDES.TWITTER}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width={iconSize} height={iconSize} fill="currentColor" className="bi bi-twitter-x" viewBox="0 0 16 16">
+                                <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
+                            </svg>
+                        </a>
                     </Col>
                     <Col className='col text-center' xs='1' md={1}>
-                        <Instagram size={iconSize} href={REDES.INSTAGRAM}></Instagram>
+                        <a href={REDES.INSTAGRAM}><Instagram size={iconSize}></Instagram></a>
                     </Col>
                     <Col className='col text-center' xs='1' md={1}>
-                        <Tiktok size={iconSize}></Tiktok>
+                        <a href={REDES.TIKTOK}><Tiktok size={iconSize}></Tiktok></a>
                     </Col>
                     <Col className='col text-center' xs='1' md={1}>
-                        <Facebook size={iconSize}></Facebook>
+                        <a href={REDES.FACEBOOK}><Facebook size={iconSize}></Facebook></a>
                     </Col>
                     <Col className='col text-center' xs='1' md={1}>
-                        <Youtube size={iconSize}></Youtube>
+                        <a href={REDES.YOUTUBE}><Youtube size={iconSize}></Youtube></a>
                     </Col>
                     <Col className='col text-center' xs='1' md={1}>
-                        <Envelope size={iconSize}></Envelope>
+                        <a href={REDES.EMAIL}><Envelope size={iconSize}></Envelope></a>
                     </Col>
                     <Col className='col text-center' xs='1' md={1}>
-                        <Whatsapp size={iconSize}></Whatsapp>
+                        <a href={REDES.WHATSAPP}><Whatsapp size={iconSize}></Whatsapp></a>
                     </Col>
                 </Row>
                 <Row className='mb-2'>
