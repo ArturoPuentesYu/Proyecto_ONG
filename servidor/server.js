fdf38d6b4cb6bd9c6c49204b869e7b9355c82139
Victor <maurogonzalezvictor@gmail.com>
2024-02-13 14:18:18 +0100

    Intento crear servidor

---
+++ servidor/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const {MongoClient} = require("mongodb");
+const cors = require("cors");
+const url = "mongodb+srv://lectura:lectura1234@clusterprueba.esmqyee.mongodb.net/?retryWrites=true&w=majority";
+const bbdd = "ong";
+
+const app = express();
+app.use(cors());
+
+app.get("/", async (req, res) => {
+    const cliente = new MongoClient(url);
+    let t = "";
+    try {
+        await cliente.connect();
+        console.log("Conectado a la base de datos");
+        t = await cliente.db(bbdd).collection("inicio").find({}).toArray();
+        console.log(t);
+    } catch (error) {
+        console.log(error);
+    } finally{
+        res.send(t);
+    }
+    cliente.close();
+})
+
+app.listen(3000, () => {console.log("Escuchando el puerto 3000")});
