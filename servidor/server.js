f513e070c7ed6184ab04f0bca47f8250110c95aa
Victor <maurogonzalezvictor@gmail.com>
2024-02-16 20:29:36 +0100

    Login resuelto

--- servidor/server.js
+++ servidor/server.js
@@ -6,6 +6,7 @@ const bcrypt = require("bcrypt");
 const url = "mongodb+srv://lectura:lectura1234@clusterprueba.esmqyee.mongodb.net/?retryWrites=true&w=majority";
 const bbdd = "ong";
 const bodyParser = require("body-parser");
+const jwt = require('jsonwebtoken');
 
 const app = express();
 app.use(cors());
@@ -31,16 +32,7 @@ app.get("/", async (req, res) => {
 });
 
 app.put("/", async (req, res) => {
-    /*
-    
-    {
-  ruta: 'JSON.INICIO.TEXTO_ONG',
-  texto: 'Las iniciales ONG significan <b>Organización No Gubernamental</b>. Se utiliza paraidentificar a organizaciones o asociaciones que tengan objetivos sociales pero noestén asociadas a ningún gobierno.<br>La Asociación Juvenil por el Ocio Alternativo Tres Cantos (AJ3C) es una entidad sin ánimo de lucro que lleva desde 2007 actuando en #TresCantos y colaborando con el #Ayuntamiento. Nuestro objetivo es ofrecer un ocio alternativo, sano y asequible a los jóvenes del municipio, además de apoyarles en sus proyectos. Somos una familia en crecimiento. Contamos con casi 2.000 socios oficiales y 60 voluntarios activos permanentemente. Los socios no pagan cuotas mensuales ni reciben remuneración por su trabajo. Al principio, la asociación se nutría de subvenciones públicas pero por suerte, desde hace unos años, se autogestiona gracias a los ingresos de los servicios ofrecidos a personas y entidades privadas y públicas.',
-  seccion: null,
-  item: null
-}
-    
-    */
+
     const descripcion = req.body.texto;
     const cliente = new MongoClient(url);
     let t = "";
@@ -77,37 +69,41 @@ app.get("/quienes_somos", async (req, res) => {
 
 /*app.get('/admin', verificarToken, (req, res) => {
     res.send("Bienvenido al panel de administración");
-});*/
+});
+// Define un esquema y modelo para los usuarios
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String,
+});
+const User = mongoose.model('User', userSchema);*/
 
 app.post('/login', async (req, res) => {
     // Intenta encontrar un usuario en la base de datos que coincida con el correo electrónico proporcionado
-    const user = await User.findOne({ email: req.body.email });
+    let user = null;
+    try {
+        const client = new MongoClient(url);
+        await client.connect();
+        const db = client.db(bbdd);
+        const collection = db.collection("user");
+        user = await collection.findOne({ email: req.body.email });
+    } catch (error) {
+    }
     // Si se encuentra un usuario con el correo electrónico proporcionado...
-    if (user) {
-        // Compara la contraseña proporcionada con la contraseña hasheada almacenada usando bcrypt
-        const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (user !== null) {
+        const validPassword = bcrypt.compareSync(req.body.password, user.password);
         if (validPassword) {
-            // Genera un token JWT que contiene el ID del usuario y tiene una duración de 1 hora
             const token = jwt.sign({ id: user._id }, 'tuSecretSuperSecreto', { expiresIn: '1h' });
-            // Envía una respuesta con el token JWT y un mensaje de éxito
             res.status(200).json({ message: "Login exitoso", token, ok: true });
         } else {
             res.status(400).json({ error: "Contraseña incorrecta", ok: false });
         }
     } else {
-        // Si no se encuentra un usuario con el correo electrónico proporcionado, envía un mensaje de error
         res.status(404).json({ error: "Usuario no encontrado", ok: false });
     }
 });
 
 
 
-// Define un esquema y modelo para los usuarios
-const userSchema = new mongoose.Schema({
-    email: String,
-    password: String,
-});
-const User = mongoose.model('User', userSchema);
 
 const verificarToken = (req, res, next) => {
     const token = req.headers['authorization']?.split(' ')[1]; // Bearer Token
